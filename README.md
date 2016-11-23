326924a616e7c4571253222f92390eca2992dd53
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-11-23 14:37:14 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,169 @@
 # parse-favicon
-Parse HTML to get favicon information
+
+[![npm](https://img.shields.io/npm/v/parse-favicon.svg?maxAge=2592000)](https://www.npmjs.com/package/parse-favicon)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/BlackGlory/parse-favicon/master/LICENSE)
+
+Parse HTML to get favicon information.
+
+## Usage
+
+```js
+parseFavicon(
+  html: string
+, options: Object = {
+    baseURI: URI = ''
+  , allowUseNetwork: boolean = false
+  , allowParseImage: boolean = false
+  }
+, ignoreException: boolean = false
+) : Promise
+```
+
+### Example
+
+```js
+const parseFavicon = require('parse-favicon')
+const axios = require('axios')
+
+axios.get('https://github.com')
+.then(({ data: html }) => parseFavicon(html, { baseURI: 'https://github.com', allowUseNetwork: true, allowParseImage: true }))
+.then(console.log)
+.catch(console.error)
+/*
+[ { url: 'https://github.com/windows-tile.png',
+  path: '/windows-tile.png',
+  size: '512x512',
+  type: 'png',
+  refer: 'msapplication-TileImage' },
+  ...
+  { url: 'https://github.com/favicon.ico',
+    path: '/favicon.ico',
+    size: '16x16',
+    type: 'image/x-icon',
+    refer: '/favicon.ico' } ]
+ */
+```
+
+### CLI
+
+```sh
+> node ./src/parse-favicon.js "https://github.com"
+
+[
+  [
+    {
+      "url": "https://github.com/windows-tile.png",
+      "path": "/windows-tile.png",
+      "size": "512x512",
+      "type": "png",
+      "refer": "msapplication-TileImage"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon.png",
+      "path": "/apple-touch-icon.png",
+      "size": "120x120",
+      "type": "png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-57x57.png",
+      "path": "/apple-touch-icon-57x57.png",
+      "size": "57x57",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-60x60.png",
+      "path": "/apple-touch-icon-60x60.png",
+      "size": "60x60",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-72x72.png",
+      "path": "/apple-touch-icon-72x72.png",
+      "size": "72x72",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-76x76.png",
+      "path": "/apple-touch-icon-76x76.png",
+      "size": "76x76",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-114x114.png",
+      "path": "/apple-touch-icon-114x114.png",
+      "size": "114x114",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-120x120.png",
+      "path": "/apple-touch-icon-120x120.png",
+      "size": "120x120",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-144x144.png",
+      "path": "/apple-touch-icon-144x144.png",
+      "size": "144x144",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-152x152.png",
+      "path": "/apple-touch-icon-152x152.png",
+      "size": "152x152",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://github.com/apple-touch-icon-180x180.png",
+      "path": "/apple-touch-icon-180x180.png",
+      "size": "180x180",
+      "type": "image/png",
+      "refer": "apple-touch-icon"
+    },
+    {
+      "url": "https://assets-cdn.github.com/favicon.ico",
+      "path": "/favicon.ico",
+      "size": "16x16",
+      "type": "image/x-icon",
+      "refer": "icon"
+    },
+    {
+      "url": "https://github.com/favicon.ico",
+      "path": "/favicon.ico",
+      "size": "16x16",
+      "type": "image/x-icon",
+      "refer": "/favicon.ico"
+    }
+  ]
+]
+```
+
+## Installation
+
+### NPM
+
+```sh
+npm install parse-favicon
+```
+
+### Yarn
+
+```sh
+yarn add parse-favicon
+```
+
+## Related Projects
+
+[BlackGlory/ico-size: A Node module to get dimensions of ico & cur image file](https://github.com/BlackGlory/ico-size)
+
+## References
+
+https://github.com/audreyr/favicon-cheat-sheet
---
+++ src/parse-favicon.js
@@ -0,0 +1,198 @@
+const cheerio = require('cheerio')
+const axios = require('axios')
+const url = require('url')
+const mime = require('mime-types')
+const co = require('co')
+const sizeOf = require('image-size')
+const icoSizeOf = require('ico-size')
+
+const parseFavicon = co.wrap(function *(html, { baseURI = '', allowUseNetwork = false, allowParseImage = false }, ignoreException = false) {
+  function addRefer(refer) {
+    return function(obj) {
+      return Object.assign({}, obj, { refer })
+    }
+  }
+
+  function isAbsoluteURL(urlObject) {
+    if (typeof urlObject === 'string') {
+      urlObject = url.parse(urlObject)
+    }
+    return !!urlObject.host
+  }
+
+  const createIcon = co.wrap(function *(uri, type = mime.lookup(uri), size) {
+    let result = {}
+
+    Object.assign(result, ((baseURI, uri) => {
+      if (isAbsoluteURL(uri)) {
+        return { url: uri, path: url.parse(uri).pathname }
+      } else {
+        if (baseURI) {
+          let absoluteURL = url.resolve(baseURI, uri)
+          if (isAbsoluteURL(absoluteURL)) {
+            return { url: absoluteURL, path: uri }
+          }
+        }
+        return { path: uri }
+      }
+    })(baseURI, uri))
+
+    if (size && /^\d+x\d+$/.test(size)) {
+      Object.assign(result, { size })
+    } else if (result.url) {
+      try {
+        let { data, headers: { 'content-type': type }} = yield axios.get(result.url, { responseType: 'arraybuffer' })
+
+        let width, height
+        try {
+          if (mime.extension(type) === 'ico') {
+            let info = icoSizeOf(data)
+            width = info.width
+            height = info.height
+          } else {
+            let info = sizeOf(data)
+            width = info.width
+            height = info.height
+            type = info.type
+          }
+        } catch(e) {
+          throw e
+        }
+        if (width && height) {
+          Object.assign(result, { size: `${ width }x${ height }` })
+        }
+
+        if (type) {
+          Object.assign(result, { type })
+        }
+      } catch(e) {
+        if (!ignoreException) {
+          throw e
+        }
+      }
+    }
+
+    if (!result.type) {
+      Object.assign(result, { type })
+    }
+
+    return result
+  })
+
+  const $ = cheerio.load(html)
+
+  let msapplicationTileImage = yield co(function *() {
+    let icons = []
+
+    for (let el of $('meta[name="msapplication-TileImage"][content]').toArray()) {
+      let content = $(el).attr('content')
+      let sizes = $(el).attr('sizes')
+      try {
+        icons.push(yield createIcon(content, undefined, sizes))
+      } catch(e) {
+        if (!ignoreException) {
+          throw e
+        }
+      }
+    }
+
+    return icons
+  })
+
+  let appleTouchIconPrecomposed = yield co(function *() {
+    let icons = []
+
+    for (let el of $('link[rel="apple-touch-icon-precomposed"][href]').toArray()) {
+      let href = $(el).attr('href')
+      let sizes = $(el).attr('sizes')
+      try {
+        icons.push(yield createIcon(href, undefined, sizes))
+      } catch(e) {
+        if (!ignoreException) {
+          throw e
+        }
+      }
+    }
+
+    return icons
+  })
+
+  let appleTouchIcons = yield co(function *() {
+    let icons = []
+
+    for (let el of $('link[rel="apple-touch-icon"][href]').toArray()) {
+      let href = $(el).attr('href')
+      let sizes = $(el).attr('sizes')
+      try {
+        icons.push(yield createIcon(href, undefined, sizes))
+      } catch(e) {
+        if (!ignoreException) {
+          throw e
+        }
+      }
+    }
+
+    return icons
+  })
+
+  let icons = yield co(function *() {
+    let icons = []
+
+    for (let el of $('link[rel~="icon"][href]').toArray()) {
+      let href = $(el).attr('href')
+      let sizes = $(el).attr('sizes')
+      try {
+        icons.push(yield createIcon(href, undefined, sizes))
+      } catch(e) {
+        if (!ignoreException) {
+          throw e
+        }
+      }
+    }
+
+    return icons
+  })
+
+  let defaultPathFavicon = []
+
+  if (allowUseNetwork) {
+    if (baseURI) {
+      const FAVICON_DEFAULT_PATH = '/favicon.ico'
+      let faviconURI = url.resolve(baseURI, FAVICON_DEFAULT_PATH)
+      if (!icons.find(x => x.absoluteURL === faviconURI)) {
+        try {
+          let { headers: { 'content-type': contentType }} = yield axios.get(faviconURI)
+          if (mime.extension(contentType) === 'ico') {
+            defaultPathFavicon.push(yield createIcon(FAVICON_DEFAULT_PATH, contentType))
+          }
+        } catch(e) {
+          if (!ignoreException) {
+            throw e
+          }
+        }
+      }
+    }
+  }
+
+  return [
+    ...msapplicationTileImage.map(addRefer('msapplication-TileImage'))
+  , ...appleTouchIcons.map(addRefer('apple-touch-icon'))
+  , ...appleTouchIconPrecomposed.map(addRefer('apple-touch-icon-precomposed'))
+  , ...icons.map(addRefer('icon'))
+  , ...defaultPathFavicon.map(addRefer('/favicon.ico'))
+  ]
+})
+
+if (require.main === module) {
+  const [,, ...urls] = process.argv
+  Promise.all(urls.map(axios.get))
+  .then(results => Promise.all(
+    results
+    .map(x => parseFavicon(x.data, { baseURI: x.config.url, allowUseNetwork: true, allowParseImage: true }, true))
+  ))
+  .then(x => JSON.stringify(x, undefined, 2))
+  .then(console.log)
+  .catch(console.error)
+} else {
+  module.exports = parseFavicon
+}
