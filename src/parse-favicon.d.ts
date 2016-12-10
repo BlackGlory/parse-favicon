925490cf2ec273e2218f02860cd86637c13c3a66
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2016-12-10 14:13:32 +0800

    1.1.1
    Add d.ts file.

---
+++ src/parse-favicon.d.ts
@@ -0,0 +1,20 @@
+declare module "parse-favicon" {
+  interface IconInfo{
+    url: string
+    path: string
+    size: string
+    type: string
+    refer: string
+  }
+
+  interface ParseOptions {
+    baseURI?: string
+    allowUseNetwork?: boolean
+    allowParseImage?: boolean
+    timeout?: number
+  }
+
+  let parseFavicon: (html: string, options?: ParseOptions, ignoreException?: boolean) => PromiseLike<IconInfo[]>
+
+  export = parseFavicon
+}
