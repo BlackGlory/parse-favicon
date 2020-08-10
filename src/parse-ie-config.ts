845d3f3a7cdc4fecda1386528d93aee300b45d50
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-08-10 20:58:05 +0800

    refactor: reduce lines

--- src/parse-ie-config.ts
+++ src/parse-ie-config.ts
@@ -12,21 +12,23 @@ import { produce } from '@shared/immer'
 export async function parseIEConfig(html: string, textFetcher: TextFetcher): Promise<Icon[]> {
   const document = parseHTML(html)
   const configUrls = getConfigUrls(document)
-  const results = await map(configUrls, async url => {
+  const icons = await map(configUrls, getIconsFromUrl)
+  return icons.flatMap(x => x)
+
+  async function getIconsFromUrl(url: string): Promise<Icon[]> {
     const text = await fetch(url)
     if (text) {
       return getIEConfigIcons(text, url)
     } else {
       return []
     }
-  })
-  return ([] as Icon[]).concat(...results)
 
-  async function fetch(url: string): Promise<string | undefined> {
-    try {
-      return await textFetcher(url)
-    } catch (e) {
-      return undefined
+    async function fetch(url: string): Promise<string | undefined> {
+      try {
+        return await textFetcher(url)
+      } catch {
+        return undefined
+      }
     }
   }
 }
@@ -39,87 +41,35 @@ function getConfigUrls(document: Document): string[] {
     .toArray()
 }
 
-function getIEConfigIcons(xml: string, baseURI: string): Icon[] {
+function getIEConfigIcons(xml: string, configUrl: string): Icon[] {
   const document = parseXML(xml)
-  const icons = [
-    ...getSquare70x70Icons(document)
-  , ...getSquare150x150Icons(document)
-  , ...getWide310x150Icons(document)
-  , ...getSquare310x310Icons(document)
-  ]
-  return icons.map(iconUrlsToRelativeUrls)
+  return [
+    ...getIcons(document, '/browserconfig/msapplication/tile/square70x70logo', { width: 70, height: 70 })
+  , ...getIcons(document, '/browserconfig/msapplication/tile/square150x150logo', { width: 150, height: 150 })
+  , ...getIcons(document, '/browserconfig/msapplication/tile/wide310x150logo', { width: 310, height: 150 })
+  , ...getIcons(document, '/browserconfig/msapplication/tile/square310x310logo', { width: 310, height: 310 })
+  ].map(combineIconUrlWithConfigUrl)
 
-  function iconUrlsToRelativeUrls(icon: Icon): Icon {
-    return produce(icon, draft => {
-      draft.url = combineRelativeUrls(baseURI, icon.url)
+  function combineIconUrlWithConfigUrl(icon: Icon): Icon {
+    return produce(icon, icon => {
+      icon.url = combineRelativeUrls(configUrl, icon.url)
     })
   }
 }
 
-function getSquare70x70Icons(document: Document): Icon[] {
-  const elements = query.call(document, xpath`/browserconfig/msapplication/tile/square70x70logo`)
+function getIcons(document: Document, selector: string, size: { width: number, height: number }): Icon[] {
+  const elements = query.call(document, xpath`${selector}`)
   return new IterableOperator(elements)
     .transform(transformElementToAttr('src'))
-    .map(createSquare70x70Icon)
+    .map(url => createIcon(url, size))
     .toArray()
 
-  function createSquare70x70Icon(url: string): Icon {
+  function createIcon(url: string, size: { width: number, height: number }): Icon {
     return {
-      url
-    , reference: 'msapplication-config'
-    , type: undefined
-    , size: { width: 70, height: 70 }
-    }
-  }
-}
-
-function getSquare150x150Icons(document: Document): Icon[] {
-  const elements = query.call(document, xpath`/browserconfig/msapplication/tile/square150x150logo`)
-  return new IterableOperator(elements)
-    .transform(transformElementToAttr('src'))
-    .map(createSquare150x150Icon)
-    .toArray()
-
-  function createSquare150x150Icon(url: string): Icon {
-    return {
-      url
-    , reference: 'msapplication-config'
-    , type: undefined
-    , size: { width: 150, height: 150 }
-    }
-  }
-}
-
-function getWide310x150Icons(document: Document): Icon[] {
-  const elements = query.call(document, xpath`/browserconfig/msapplication/tile/wide310x150logo`)
-  return new IterableOperator(elements)
-    .transform(transformElementToAttr('src'))
-    .map(createSquare310x310Icon)
-    .toArray()
-
-  function createSquare310x310Icon(url: string): Icon {
-    return {
-      url
-    , reference: 'msapplication-config'
-    , type: undefined
-    , size: { width: 310, height: 150 }
-    }
-  }
-}
-
-function getSquare310x310Icons(document: Document): Icon[] {
-  const elements = query.call(document, xpath`/browserconfig/msapplication/tile/square310x310logo`)
-  return new IterableOperator(elements)
-    .transform(transformElementToAttr('src'))
-    .map(createSquare310x310Icon)
-    .toArray()
-
-  function createSquare310x310Icon(url: string): Icon {
-    return {
-      url
-    , reference: 'msapplication-config'
-    , type: undefined
-    , size: { width: 310, height: 310 }
+        reference: 'msapplication-config'
+      , url
+      , type: undefined
+      , size
     }
   }
 }
