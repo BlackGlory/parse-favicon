255235293f0e0552b520b36eead2158f078c5b8a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-08-12 17:29:10 +0800

    refactor: add @blackglory/types

--- src/shared/is-url.ts
+++ src/shared/is-url.ts
@@ -1,7 +1,7 @@
 import { isSuccess } from 'return-style'
-import { isString } from '@shared/is-string'
+import { isString } from '@blackglory/types'
 
 export function isUrl(val: unknown): boolean {
   const base = 'http://localhost'
-  return isString(val) && isSuccess(() => new URL(val, base))
+  return isString(val) && isSuccess(() => new URL(val.toString(), base))
 }
