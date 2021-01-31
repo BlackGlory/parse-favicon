1643412e4b563768037375dfd5f9eba5a183b7d7
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-31 02:51:27 +0000

    fix: run in Node.js (#7)

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const { compilerOptions } = require('./tsconfig.base.json')
 
 module.exports = {
   preset: 'ts-jest'
+, testEnvironment: 'node'
 , testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)']
 , moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/'
