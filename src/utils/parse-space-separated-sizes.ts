884c480fcf2b510f7a1f3f042f0db463651d8da3
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-10 04:48:27 +0000

    refactor: improve readability

--- src/utils/parse-space-separated-sizes.ts
+++ src/utils/parse-space-separated-sizes.ts
@@ -1,11 +1,12 @@
 import { map, toArray } from 'iterable-operator'
 import { pipe } from 'extra-utils'
 import matchAll from 'string.prototype.matchall'
+import { ISize } from '@src/types.js'
 
-export function parseSpaceSeparatedSizes(sizes: string): Array<{
-  width: number
-  height: number
-}> {
+/**
+ * @param sizes 例子`32x32`, `32x32 64x64`
+ */
+export function parseSpaceSeparatedSizes(sizes: string): ISize[] {
   if (/^\d+[x|X]\d+(?:\s+\d+[x|X]\d+)*$/.test(sizes)) {
     const re = /(?<width>\d+)[x|X](?<height>\d+)/g
     const matchResults = matchAll(sizes, re)
