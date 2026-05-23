32fdb259564d5c764dc702efbd25669c9c2acd6b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-05-23 14:42:10 +0000

    test: add a smoke test

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -2,26 +2,40 @@ import { isntEmptyArray } from '@blackglory/prelude'
 import { parseFavicon } from '@src/index.js'
 import { toArray, lastValueFrom } from 'rxjs'
 
-test('parseFavicon', async () => {
-  const pageURL = 'https://github.com'
+describe('parseFavicon', () => {
+  test('without fetchBuffer', async () => {
+    const pageURL = 'https://github.com'
 
-  const results = await lastValueFrom(
-    parseFavicon(pageURL, fetchText, fetchBuffer)
-      .pipe(toArray())
-  )
+    const results = await lastValueFrom(
+      parseFavicon(pageURL, fetchText)
+        .pipe(toArray())
+    )
 
-  // Considering that the results may change,
-  // we only need to ensure the array is not empty.
-  // Yes, that is basically a smoke test.
-  expect(isntEmptyArray(results)).toBe(true)
+    // Considering that the results may change,
+    // we only need to ensure the array is not empty.
+    expect(isntEmptyArray(results)).toBe(true)
+  }, 30 * 1000)
 
-  function fetchText(url: string): Promise<string> {
-    return fetch(url)
-      .then(res => res.text())
-  }
+  test('with fetchBuffer', async () => {
+    const pageURL = 'https://github.com'
 
-  function fetchBuffer(url: string): Promise<ArrayBuffer> {
-    return fetch(url)
-      .then(res => res.arrayBuffer())
-  }
-}, 30 * 1000)
+    const results = await lastValueFrom(
+      parseFavicon(pageURL, fetchText, fetchBuffer)
+        .pipe(toArray())
+    )
+
+    // Considering that the results may change,
+    // we only need to ensure the array is not empty.
+    expect(isntEmptyArray(results)).toBe(true)
+  }, 30 * 1000)
+})
+
+function fetchText(url: string): Promise<string> {
+  return fetch(url)
+    .then(res => res.text())
+}
+
+function fetchBuffer(url: string): Promise<ArrayBuffer> {
+  return fetch(url)
+    .then(res => res.arrayBuffer())
+}
